284d1c9ef98302b7b4d97003dc63f78078df0360
manuelhernandezcarrasco <manuel.hernandez@ing.austral.edu.ar>
2024-09-12 03:07:22 -0300

    1.1.2

--- cli/src/main/kotlin/cli/CLIInputProvider.kt
+++ cli/src/main/kotlin/cli/CLIInputProvider.kt
@@ -4,7 +4,6 @@ import utils.InputProvider
 
 class CLIInputProvider : InputProvider {
     override fun readInput(message: String): String? {
-        println(message)
         return readlnOrNull()
     }
 }
